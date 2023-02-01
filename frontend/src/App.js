ceb1d7d533b3d7edcb31d64d2f368c4eb958b7f6
Leandro Coutinho <franciscoleandrosilvacoutinho@gmail.com>
2023-01-31 22:27:27 -0300

    Add sass

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import './styles/globals.scss';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -9,19 +10,19 @@ function App() {
         <form >
           <div class="box">
             <input id="nota1" type="radio" name="nota_atendimento" value="1"/>
-            <label for="nota1" ><img src="assets/1.png" alt="nota 1"/></label>
+            <label for="nota1" ><img src="../assets/1.png" alt="nota 1"/></label>
 
             <input id="nota2" type="radio" name="nota_atendimento" value="2"/>
-            <label for="nota2" ><img src="assets/2.png"/></label>
+            <label for="nota2" ><img src="assets/2.png" alt="nota 2"/></label>
 
             <input id="nota3" type="radio" name="nota_atendimento" value="3"/>
-            <label for="nota3" ><img src="assets/3.png"/></label>
+            <label for="nota3" ><img src="assets/3.png" alt="nota 3"/></label>
 
             <input id="nota4" type="radio" name="nota_atendimento" value="4"/>
-            <label for="nota4" ><img src="assets/4.png"/></label>
+            <label for="nota4" ><img src="assets/4.png" alt="nota 4"/></label>
 
             <input id="nota5" type="radio" name="nota_atendimento" value="5"/>      
-            <label for="nota5" ><img src="assets/5.png"/></label>
+            <label for="nota5" ><img src="assets/5.png" alt="nota 5"/></label>
           </div>
           <div class="sombra"></div>
         </form>
