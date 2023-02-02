87737321e96b900e8946899152431c3d0c96f043
Leandro Coutinho <franciscoleandrosilvacoutinho@gmail.com>
2023-02-01 22:34:35 -0300

    Load image

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,7 +1,7 @@
 export default function Header(){
     return(
         <header>
-            <img src="../../assets/logo.png" class="logo" alt="Logo da empresa Golub"/>
+            <img src="logo.png" class="logo" alt="Logo da empresa Golub"/>
             <h1>PESQUISA DE SATISFAÇÃO</h1>
             <h3>Atendimento</h3>
         </header>
