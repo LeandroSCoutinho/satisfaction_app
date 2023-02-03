43c8e68be241477da08efe9b26876ee05c823af6
Leandro Coutinho <franciscoleandrosilvacoutinho@gmail.com>
2023-02-02 23:06:33 -0300

    Style Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './styles/globals.scss';
+import styles from "./styles/home.module.scss";
 
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -6,27 +7,29 @@ import Footer from './components/Footer';
 function App() {
   return (
     <>
-     <Header/>
-        <form >
-          <div class="box">
-            <input id="nota1" type="radio" name="nota_atendimento" value="1"/>
-            <label for="nota1" ><img src="1.png" alt="nota 1"/></label>
+    <main className={styles.container}>
+        <Header/>
+          <form >
+            <div class="box">
+              <input id="nota1" type="radio" name="nota_atendimento" value="1"/>
+              <label for="nota1" ><img src="1.png" alt="nota 1"/></label>
 
-            <input id="nota2" type="radio" name="nota_atendimento" value="2"/>
-            <label for="nota2" ><img src="2.png" alt="nota 2"/></label>
+              <input id="nota2" type="radio" name="nota_atendimento" value="2"/>
+              <label for="nota2" ><img src="2.png" alt="nota 2"/></label>
 
-            <input id="nota3" type="radio" name="nota_atendimento" value="3"/>
-            <label for="nota3" ><img src="3.png" alt="nota 3"/></label>
+              <input id="nota3" type="radio" name="nota_atendimento" value="3"/>
+              <label for="nota3" ><img src="3.png" alt="nota 3"/></label>
 
-            <input id="nota4" type="radio" name="nota_atendimento" value="4"/>
-            <label for="nota4" ><img src="4.png" alt="nota 4"/></label>
+              <input id="nota4" type="radio" name="nota_atendimento" value="4"/>
+              <label for="nota4" ><img src="4.png" alt="nota 4"/></label>
 
-            <input id="nota5" type="radio" name="nota_atendimento" value="5"/>      
-            <label for="nota5" ><img src="5.png" alt="nota 5"/></label>
-          </div>
-          <div class="sombra"></div>
-        </form>
-        <Footer/> 
+              <input id="nota5" type="radio" name="nota_atendimento" value="5"/>      
+              <label for="nota5" ><img src="5.png" alt="nota 5"/></label>
+            </div>
+            <div class="sombra"></div>
+          </form>
+          <Footer/> 
+        </main>
     </>
   );
 }
--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,9 +1,10 @@
+import styles from "./style.module.scss";
 export default function Header(){
     return(
-        <header>
-            <img src="logo.png" class="logo" alt="Logo da empresa Golub"/>
+        <header className={styles.headerContainer}>
+            <img src="logo.png" className={styles.logo} alt="Logo da empresa Golub"/>
             <h1>PESQUISA DE SATISFAÇÃO</h1>
-            <h3>Atendimento</h3>
+            <h3>Queremos lhe ouvir, dê a sua nota!</h3>
         </header>
     )
 }
